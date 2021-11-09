33cc93f77be8d850ad2eef7c04ed7c848eebfefd
heartbeat98 <sbhaskar1998@gmail.com>
2021-11-09 23:19:59 +0530

    Added account page
    some functionalities needs to be implemented.

--- account.js
+++ account.js
@@ -3,28 +3,55 @@ document.getElementById("next").addEventListener("click", accountCheck);
 var acc = JSON.parse(localStorage.getItem("accDetails")) || [];
 function accountCheck() {
     var x = document.getElementById("email").value;
-     var pattern = /^[6-9]\d{9}$/;
-     var pattern2 = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-    // validating Mobile No
-     if (pattern.test(x)) { 
+    var pattern = /^[6-9]\d{9}$/;
+    var pattern2 = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+
+    if (pattern.test(x)) /* validating mobile */ {
         var mobObj = {
             mobileNo: x,
         };
-        acc.push(mobObj);
+        if (acc.length !== 0) {                      /* Checking if the user has already registered or not
+                                                      if not than taking him to registration page */
+            for (var i = 0; i < acc.length; i++) {
+                if (x == acc[i].mobileNo) {
+                    console.log("some code will go here");
+                }
+                else {
+                    acc.push(mobObj);
+                    window.location.href = "register.html";
+                }
+            }
+        }
+        else {
+            acc.push(mobObj);
+            window.location.href = "register.html";
+        }
+
     }
-    //validating Email
-    else if(pattern2.test(x)) {
+    else if (pattern2.test(x)) /*validating email*/ {
         var emailObj = {
             email: x,
         };
-        acc.push(emailObj);
+        if (acc.length !== 0) {
+            for (var i = 0; i < acc.length; i++) {
+                if (x == acc[i].email) {
+                    console.log("some code will go here");
+                }
+                else {
+                    acc.push(emailObj);
+                    window.location.href = "register.html";
+                }
+            }
+        }
+        else {
+            acc.push(emailObj);
+            window.location.href = "register.html";
+        }
     }
-    else{
+    else {
         alert("Error: Check the Email/Mobile No. entered");
     }
-    // acc.push(mobObj, emailObj);
     localStorage.setItem("accDetails", JSON.stringify(acc));
-    console.log(acc.length);
 }
 
 // function validateMobile(x) {
