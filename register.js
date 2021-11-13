ddb864984a7677bcc883aec3f142568d553061ed
heartbeat98 <sbhaskar1998@gmail.com>
2021-11-13 13:09:51 +0530

    updated

--- register.js
+++ register.js
@@ -1,4 +1,7 @@
+<<<<<<< Updated upstream
 // adding pre-entered email in form
 var temp = localStorage.getItem("tempMail");
 document.getElementById("email_address").setAttribute("placeholder", temp)
 document.getElementById("email_address").setAttribute("value", temp)
+=======
+>>>>>>> Stashed changes
