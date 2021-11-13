096e2ca009b6fbea0a15b521745b770f8905925b
Mujaffar_H <mujaffarhssn@gmail.com>
2021-11-13 13:42:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
-document.querySelector(".giftpage").addEventListener("click",function(){
-    window.location.href="gift.html"
-})
+// document.querySelector(".giftpage").addEventListener("click",function(){
+//     window.location.href="gift.html"
+// })
 
 
 var slideIndex = 1;
